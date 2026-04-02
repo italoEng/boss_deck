32603919fe2015c24d89bf9f19540b275af1ae7a
italoEng <franca.italo05@gmail.com>
2026-04-02 10:57:31 -0300

    teste product

--- main.py
+++ main.py
@@ -94,4 +94,5 @@ def card_excluir(deck_id, card_id):
     return redirect(f"/deck/{deck_id}#lista")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
